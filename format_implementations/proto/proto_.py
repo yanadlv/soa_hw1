ece774e467bcf4e248a04ef3bdca59eede73753b
yana <74400071+yanadlv@users.noreply.github.com>
2023-05-01 05:55:26 +0300

    better field assignment

--- format_implementations/proto/proto_.py
+++ format_implementations/proto/proto_.py
@@ -4,8 +4,9 @@ import data_pb2
 def serialize(data):
     res = data_pb2.Data()
     res.string = data["string"]
-    res.list = data["list"]
-    res.dict = data["dict"]
+    res.list.extend(data["list"])
+    for key, value in data["dict"].items():
+        res.dict[key] = value
     res.int = data["int"]
     res.float = data["float"]
     return res.SerializeToString()
