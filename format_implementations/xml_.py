770c0ffe11b4f3340a4d2103bd25590fcce29bf1
yana <74400071+yanadlv@users.noreply.github.com>
2023-05-01 05:54:52 +0300

    typo fix

--- format_implementations/xml_.py
+++ format_implementations/xml_.py
@@ -3,7 +3,7 @@ import xml.etree.ElementTree as ET
 
 def serialize(data):
     root = ET.Element("data")
-    for key, value in data:
+    for key, value in data.items():
         child = ET.Element(key)
         child.text = str(value)
         root.append(child)
